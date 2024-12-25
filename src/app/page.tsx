0538c9491e400d726009b478be7e82b0561f7702
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-12-25 11:56:02 +0530

    feat: create welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className='bg-green-200 p-5 rounded-md w-2/4 m-4 text-center mx-auto'>
+         <h1 className="text-2xl font-bold text-center">
+             Task Management System
+         </h1>
+      </div>
   );
 }
